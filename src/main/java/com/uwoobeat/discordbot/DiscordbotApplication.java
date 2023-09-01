e798a1363fb7accd3a221ad05cd25cb5c91abd10
uwoobeat <desktop3711@gmail.com>
2023-09-01 21:33:40 +0900

    feat: 디스코드 봇 빌드

--- src/main/java/com/uwoobeat/discordbot/DiscordbotApplication.java
+++ src/main/java/com/uwoobeat/discordbot/DiscordbotApplication.java
@@ -1,5 +1,11 @@
 package com.uwoobeat.discordbot;
 
+import com.uwoobeat.discordbot.discord.listener.CustomDiscordListener;
+import com.uwoobeat.discordbot.global.utils.DiscordTokenUtil;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +14,12 @@ public class DiscordbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscordbotApplication.class, args);
+
+		JDA jda = JDABuilder.createDefault(DiscordTokenUtil.token)
+				.setActivity(Activity.playing("with JDA"))
+				.enableIntents(GatewayIntent.MESSAGE_CONTENT)
+				.addEventListeners(new CustomDiscordListener())
+				.build();
 	}
 
 }
