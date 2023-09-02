e9d2b9f3cef05733eee8be5adfc3bb0fc2c7c49d
uwoobeat <desktop3711@gmail.com>
2023-09-02 22:51:48 +0900

    refactor: 변수 분리

--- src/main/java/com/uwoobeat/discordbot/discord/listener/MemberJoinListener.java
+++ src/main/java/com/uwoobeat/discordbot/discord/listener/MemberJoinListener.java
@@ -4,6 +4,8 @@ import com.uwoobeat.discordbot.domain.member.MemberService;
 import com.uwoobeat.discordbot.global.annotations.Listener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -19,7 +21,10 @@ public class MemberJoinListener extends ListenerAdapter {
         log.info("Member {} joined", event.getMember().getEffectiveName());
 
         if (!memberService.checkMembershipFeePaid()) {
-            event.getMember().kick().queue();
+            Member toBeKickedMember = event.getMember();
+            toBeKickedMember.kick().queue();
+            TextChannel channel = event.getGuild().getDefaultChannel().asTextChannel();
+            channel.sendMessage(toBeKickedMember.getEffectiveName() + " have not paid membership fee!").queue();
         }
     }
 }
